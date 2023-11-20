0f53f288c2fd0e0898d40c6ed2b79c808ef9b258
AdeDesmond <desmondade09@gmail.com>
2023-11-21 01:29:25 +0800

    completed website for a portfolio

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const hamburgerContainer = document.querySelector("#header__main-nav");
+const hamburger = document.querySelector(".header__main-nav--hamburger");
+const links = document.querySelectorAll(".header__main-nav--links li");
+
+hamburger.addEventListener("click", () => {
+  console.log("clicked");
+  hamburgerContainer.classList.toggle("clicked");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+});
